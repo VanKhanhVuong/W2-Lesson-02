042ac778cc7fe4963bb404732c5db86c50958004
VanKhanhVuong <vankhanhvuong98@gmail.com>
2021-03-22 14:22:59 +0700

    Initial Commit

---
+++ W2_Lesson-02Tests/W2_Lesson_02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  W2_Lesson_02Tests.swift
+//  W2_Lesson-02Tests
+//
+//  Created by Văn Khánh Vương on 22/03/2021.
+//
+
+import XCTest
+@testable import W2_Lesson_02
+
+class W2_Lesson_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
